f51f560e3ece3dbc7eb268f1a3fda68e7cbbd13f
CCimenci <cagricimenci@gmail.com>
2018-11-20 19:50:42 +0300

    bower and angular

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LiveBalls' });
+  res.render('index', { title: 'Live Balls' });
 });
 
 module.exports = router;
+
+
+
+
